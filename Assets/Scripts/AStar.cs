5d6d14c626d1cd6a0f73ac73f22513d637ca0455
Dmitry Lavrinov <otto.obito@gmail.com>
2021-02-06 17:48:15 +0400

    Начальное поле с кнопками. Уровень можно строить.

---
+++ Assets/Scripts/AStar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum TileType { START, GOAL, WATER, GRASS, PATH }
+
+public class AStar : MonoBehaviour
+{
+    TileType tileType;
+    [SerializeField] Tilemap tileMap;
+    // Храним все возможные тайлы
+    [SerializeField] Tile[] tiles;
+    // объект Main Camera
+    [SerializeField] Camera m_camera; 
+    // сетка игрового мира
+    [SerializeField] LayerMask layerMask;
+
+    void Update()
+    {
+        // если нажали правой кнопкой мыши
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(m_camera.ScreenToWorldPoint(Input.mousePosition), 
+                Vector2.zero, Mathf.Infinity, layerMask);
+
+            // если кликнули по объекту
+            if (hit.collider != null) 
+            {
+                // переход к мировой системе координат
+                Vector3 mouseWorldPos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int clickPos = tileMap.WorldToCell(mouseWorldPos);
+
+                ChangeTile(clickPos);
+            }
+        }
+    }
+
+    // меняет плитку уровня
+    void ChangeTile(Vector3Int clickPos)
+    {
+        tileMap.SetTile(clickPos, tiles[(int)tileType]);
+    }
+
+    // меняет тип устанавливаемой плитки
+    public void ChangeTileType(TileButton tileButton)
+    {
+        tileType = tileButton.tileType;
+    }
+}
---
+++ Assets/Scripts/AStarTile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New AStarTile", menuName ="tiles/AStarTile")]
+public class AStarTile : Tile
+{
+
+}
